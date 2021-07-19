a39a859fdb2932bff0e3565d4365ed552c8da9e6
raymanza <naran.m@ku.th>
2021-07-19 10:50:01 +0700

    producer /trial

--- src/main/java/com/test/backend/api/ApiService.java
+++ src/main/java/com/test/backend/api/ApiService.java
@@ -1,11 +1,23 @@
 package com.test.backend.api;
 
 import com.test.backend.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
 public class ApiService {
+
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+    private String topic = "test";
+
+    @GetMapping("/publish/{message}")
+    public String message(@PathVariable("message") String message){
+        kafkaTemplate.send(topic, message);
+        return "Success";
+    }
 
     @GetMapping
     public User test(){
